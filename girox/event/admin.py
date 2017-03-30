379d360dc89fd8d37b4e399f15b077c818fe08b9
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2017-03-30 20:19:55 -0300

    fix

--- girox/event/admin.py
+++ girox/event/admin.py
@@ -3,17 +3,17 @@ from django.contrib import admin
 from girox.event.models import Event, Subscription
 
 
-# class SubscriptionInline(admin.TabularInline):
-#     model = Subscription
-#     extra = 0
+class SubscriptionInline(admin.TabularInline):
+    model = Subscription
+    extra = 0
 
 
 class EventModelAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
     search_fields = ('title', 'description')
-    # inlines = [
-    #     SubscriptionInline,
-    # ]
+    inlines = [
+        SubscriptionInline,
+    ]
 
 
 admin.site.register(Event, EventModelAdmin)
