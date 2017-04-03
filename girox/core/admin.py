6098fbf94428eec8576cc18920603584fd032bce
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2017-04-02 22:45:32 -0300

    LogEntry in admin

--- girox/core/admin.py
+++ girox/core/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.admin.models import LogEntry
 
 
 admin.site.site_header = _('GiroX / Esportes')
 admin.site.site_title = _('GiroX')
 admin.site.index_title = 'Início'
+
+
+class LogEntryModelAdmin(admin.ModelAdmin):
+    list_filter = ('user',)
+    date_hierarchy = 'action_time'
+
+
+admin.site.register(LogEntry, LogEntryModelAdmin)
