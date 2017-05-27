f41dc25838f0780e1cfa74c5af9b928df6d321ac
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2017-05-27 12:07:13 -0300

    fix

--- girox/settings.py
+++ girox/settings.py
@@ -248,7 +248,7 @@ VERSATILEIMAGEFIELD_RENDITION_KEY_SETS = {
         ('modal_photo', 'thumbnail__1200x1200'),
     ],
     'advertiser': [
-        ('banner', 'thumbnail__300x250'),
+        # ('banner', 'thumbnail__300x250'),
         ('banner_crop', 'crop__300x250'),
     ],
 }
